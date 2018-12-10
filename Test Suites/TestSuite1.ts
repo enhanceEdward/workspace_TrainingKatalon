05b39971700e60915745536a5d883a6467e75763
enhanceEdward <42948388+enhanceEdward@users.noreply.github.com>
2018-12-11 10:06:18 +1300

    Further changes

---
+++ Test Suites/TestSuite1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>e5b83e35-ed5a-4ea9-be8c-1de3da61900e</testSuiteGuid>
+   <testCaseLink>
+      <guid>515e9f9e-cd2c-4716-8dd4-9c3c8dcefdbc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Record and Play</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TestSuite2.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2018-12-11T10:03:21</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>445c6384-b21f-47a3-902c-b92d26fcc5b2</testSuiteGuid>
+   <testCaseLink>
+      <guid>8d176e29-fd11-4dba-96d2-571d235df2b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Manual Test</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6f6a0f98-4329-4e54-bb18-91549ed4fe59</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Script Test</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
